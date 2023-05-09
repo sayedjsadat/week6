9d92a88e924d8b2e902e0c718085241efa47368b
Sayed Sadat <sadat.sayed.3@student.greenriver.edu>
2023-05-09 13:54:18 -0400

    added more templates

--- index.php
+++ index.php
@@ -19,14 +19,39 @@ $f3 = Base::instance();
 
 
 //Define a default route
-$f3->route('GET /', function(){
+$f3->route('GET /', function($f3){
     //echo '<h1>Hello world!</h1>';
+
+    // Add data to F3 "hive"
+    $f3->set('title', 'Welcome to Week 6: Templating');
+    $f3->set('color', 'green');
+    $f3->set('food', 'pizza');
+    $f3->set('temp', 67);
+
+    //Add a radisu variable to you controller
+    $f3->set('radius', 30);
+    //Template: Print the circumferance of a circle
+
+    // C = 2 * PI * radius
+    //PHP has a pi() function
+    // Round to 3 decimal places
+
+    $fruit = array('banana', 'apple', 'kiwi');
+    $f3->set('fruits', $fruit);
+
+    $vegies = array('Cabbage', 'Broccoli', 'Carrots');
+    $f3->set('vegetables', $vegies);
+
+
 
     //Display a view page
     $view = new Template();
     echo $view->render('views/info.html');
 });
 
-$f30->
+//Add an .htaccess file to enable routing
+//$f3->route('Get /page2', function () {
+    //echo "Page 2";
+//});
 
 $f3->run();
